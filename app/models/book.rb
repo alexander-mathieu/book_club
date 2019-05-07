ec489dabfe4648fa681c16b3006bf2deff8e007d
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-05-07 15:31:29 -0600

    Add author_names instance method to Book model with spec
    Co-authored-by: BrennanAyers <brennan.f.ayers@gmail.com>
    Co-authored-by: alexander-mathieu <alexander.mathieu@icloud.com>

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,8 @@ class Book < ApplicationRecord
   has_many :authors, through: :books_by_author
 
   validates_presence_of :title, :pages, :year
+
+  def author_names
+    authors.pluck(:name)
+  end
 end
