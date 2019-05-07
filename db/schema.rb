8845a93ed54753dd160aa0b858ba004d08d98665
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-05-07 12:25:16 -0600

    Add Review migration and update schema
    Co-authored-by: BrennanAyers <brennan.f.ayers@gmail.com>
    Co-authored-by: alexander-mathieu <alexander.mathieu@icloud.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190507181536) do
+ActiveRecord::Schema.define(version: 20190507182104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,10 +24,20 @@ ActiveRecord::Schema.define(version: 20190507181536) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.string "text"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "reviews", "users"
 end
