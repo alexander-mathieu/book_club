8977bdbad91957b8fde7b6b8b9bc2fd1dd58718d
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-05-07 12:30:26 -0600

    Add Author migration and update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190507182104) do
+ActiveRecord::Schema.define(version: 20190507182733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.string "image", default: "assets/default.png"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
