f41887f1c2d5bec37d9f258734c5471452bdbe9f
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-05-07 14:26:07 -0600

    Add BooksByAuthor relationship to Author model with spec
    Co-authored-by: BrennanAyers <brennan.f.ayers@gmail.com>
    Co-authored-by: alexander-mathieu <alexander.mathieu@icloud.com>

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Author, type: :model do
   describe "validations" do
     it {should validate_presence_of :name}
   end
+
+  describe "relationships" do
+    it {should have_many(:books).through(:books_by_author)}
+  end
 end
