6e48c31ab5989a91311e40577e8a9f1366f6c621
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-05-07 14:25:42 -0600

    Add BooksByAuthor relationship to Book model with spec
    Co-authored-by: BrennanAyers <brennan.f.ayers@gmail.com>
    Co-authored-by: alexander-mathieu <alexander.mathieu@icloud.com>

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ApplicationRecord
+  has_many :reviews
+  has_many :books_by_author
+  has_many :authors, through: :books_by_author
+
   validates_presence_of :title, :pages, :year
 end
