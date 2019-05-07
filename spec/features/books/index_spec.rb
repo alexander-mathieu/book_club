bf31cefb680612051af75949c38b327cae210773
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-05-07 15:07:54 -0600

    Add authors to book index spec

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -3,9 +3,15 @@ require 'rails_helper'
 RSpec.describe 'As a user', type: :feature do
   describe 'When I visit /books' do
     before :each do
-      @book_1 = Book.create!(title: "Veronica Mars", pages: 10, year: 2012)
-      @book_2 = Book.create!(title: "Mars Aquatic", pages: 120, year: 1964)
-      @book_3 = Book.create!(title: "Trip to Mars", pages: 480, year: 2020)
+      @author_1 = Author.create!(name: "Brennan Ayers")
+      @author_2 = Author.create!(name: "John Flapjacks")
+      @author_3 = Author.create!(name: "Patrick Duvall, M.D.")
+      @author_4 = Author.create!(name: "Alexander Mathieu")
+
+      @book_1 = @author_1.books.create!(title: "Veronica Mars", pages: 10, year: 2012)
+      @book_2 = @author_2.books.create!(title: "Mars Aquatic", pages: 120, year: 1964)
+      @book_3 = @author_3.books.create!(title: "Trip to Mars", pages: 480, year: 2020)
+      @book_1.authors << @author_4
     end
 
     it 'I see all book titles in the database' do
@@ -14,6 +20,8 @@ RSpec.describe 'As a user', type: :feature do
       within("#book-#{@book_1.id}") do
         expect(page).to have_css("img[src='#{@book_1.image}']")
         expect(page).to have_content(@book_1.title)
+        expect(page).to have_content(@book_1.authors[0].name)
+        expect(page).to have_content(@book_1.authors[1].name)
         expect(page).to have_content(@book_1.pages)
         expect(page).to have_content(@book_1.year)
       end
@@ -21,6 +29,7 @@ RSpec.describe 'As a user', type: :feature do
       within("#book-#{@book_2.id}") do
         expect(page).to have_css("img[src='#{@book_2.image}']")
         expect(page).to have_content(@book_2.title)
+        expect(page).to have_content(@book_2.authors[0].name)
         expect(page).to have_content(@book_2.pages)
         expect(page).to have_content(@book_2.year)
       end
@@ -28,6 +37,7 @@ RSpec.describe 'As a user', type: :feature do
       within("#book-#{@book_3.id}") do
         expect(page).to have_css("img[src='#{@book_3.image}']")
         expect(page).to have_content(@book_3.title)
+        expect(page).to have_content(@book_3.authors[0].name)
         expect(page).to have_content(@book_3.pages)
         expect(page).to have_content(@book_3.year)
       end
