328a040722b0eedca8f7f3640db4d5d86aa05114
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-08 17:56:18 -0600

    Add Book model Class Methods tests, with edge casing on Rating Average and Review Count
    Co-authored-by: BrennanAyers <brennan.f.ayers@gmail.com>
    Co-authored-by: alexander-mathieu <alexander.mathieu@icloud.com>

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -44,4 +44,58 @@ RSpec.describe Book, type: :model do
       expect(@book_2.review_count).to eq(1)
     end
   end
+
+  describe 'class methods' do
+    before :each do
+      @user_1 = User.create!(name: "Anony-moose")
+      @user_2 = User.create!(name: "VinnyCheese")
+
+      @author_1 = Author.create!(name: "Jim")
+      @author_2 = Author.create!(name: "Bob")
+
+      @book_1 = @author_1.books.create!(title: "The Adventure", pages: 1, year: 1985)
+      @book_2 = @author_2.books.create!(title: "Smithsonian Fun", pages: 4, year: 2017)
+      @book_3 = @author_2.books.create!(title: "Trip to Mars", pages: 480, year: 2020)
+      @book_1.authors << @author_2
+
+      @review_1 = @book_1.reviews.create!(text: "THIS BOOK IS AWESOME!", rating: 5, user: @user_1)
+      @review_2 = @book_1.reviews.create!(text: "This book didn't do it for me.", rating: 3, user: @user_2)
+      @review_3 = @book_2.reviews.create!(text: "This book seemed like it was more about drones than Mars.", rating: 2, user: @user_1)
+      @review_4 = @book_3.reviews.create!(text: "This book went to the market!", rating: 3, user: @user_2)
+    end
+
+    it '.ratings_sort' do
+      expect(Book.ratings_sort(:asc)).to eq([@book_2, @book_3, @book_1])
+      expect(Book.ratings_sort(:desc)).to eq([@book_1, @book_3, @book_2])
+    end
+
+    it '.ratings_sort with a book with no rating' do
+      book_4 = @author_1.books.create!(title: "No Reviews", pages: 1500, year: 1900)
+
+      expect(Book.ratings_sort(:asc)).to eq([book_4, @book_2, @book_3, @book_1])
+      expect(Book.ratings_sort(:desc)).to eq([@book_1, @book_3, @book_2, book_4])
+    end
+
+    it '.pages_sort' do
+      expect(Book.pages_sort(:asc)).to eq([@book_1, @book_2, @book_3])
+      expect(Book.pages_sort(:desc)).to eq([@book_3, @book_2, @book_1])
+    end
+
+    it '.reviews_sort' do
+      review_5 = @book_2.reviews.create!(text: "THIS BOOK IS!", rating: 5, user: @user_1)
+      review_6 = @book_1.reviews.create!(text: "BOOK!", rating: 5, user: @user_2)
+
+      expect(Book.reviews_sort(:desc)).to eq([@book_1, @book_2, @book_3])
+      expect(Book.reviews_sort(:asc)).to eq([@book_3, @book_2, @book_1])
+    end
+
+    it '.reviews_sort with one book with no review' do
+      review_5 = @book_2.reviews.create!(text: "THIS BOOK IS!", rating: 5, user: @user_1)
+      review_6 = @book_1.reviews.create!(text: "BOOK!", rating: 5, user: @user_2)
+      book_4 = @author_1.books.create!(title: "No Reviews", pages: 1500, year: 1900)
+
+      expect(Book.reviews_sort(:desc)).to eq([@book_1, @book_2, @book_3, book_4])
+      expect(Book.reviews_sort(:asc)).to eq([book_4, @book_3, @book_2, @book_1])
+    end
+  end
 end
