b06aee2363aae63231fc27321dda92cb842ef30e
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-05-07 19:41:14 -0600

    Add beginnings of book controller sort logic
    Co-authored-by: BrennanAyers <brennan.f.ayers@gmail.com>
    Co-authored-by: alexander-mathieu <alexander.mathieu@icloud.com>

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,14 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    if params[:rating] != nil
+      @books = @books.rating_sort(params[:rating])
+    elsif params[:pages] != nil
+      @books = @books.pages_sort(params[:pages])
+    end
   end
 end
+
+# if params[:sort] != nil
+#   @sorted_books = @books.order_by(params[:sort], params[:type])
+# end
