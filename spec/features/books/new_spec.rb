d0948f58283d16c459566e673f45692bb19f8778
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-05-09 13:04:10 -0600

    Add feature spec for user being redirected to new book show page

--- spec/features/books/new_spec.rb
+++ spec/features/books/new_spec.rb
@@ -11,7 +11,24 @@ RSpec.describe "as a user" do
         expect(page).to have_field("Year Published:")
         expect(page).to have_field("Author(s):")
         expect(page).to have_field("Image URL:")
+
+        expect(page).to have_button("Add Book")
       end
+    end
+
+    it "and click 'Add Book', I'm taken to the new book's show page" do
+      visit new_book_path
+
+      fill_in "Title:", with: "A Treatise on Dressing Casually"
+      fill_in "Number of Pages:", with: 800
+      fill_in "Year Published:", with: 2019
+      fill_in "Author(s):", with: "Zachary Livingston"
+
+      click_button "Add Book"
+
+      book = Book.last
+
+      expect(current_path).to eq(book_path(book))
     end
   end
 end
