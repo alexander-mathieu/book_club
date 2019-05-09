4c44b27bb9af00d3835be29fd304c211ad2513ef
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-09 10:11:58 -0600

    Update path helper to new_book_review, fix Author.create typo

--- spec/features/books/reviews/new_spec.rb
+++ spec/features/books/reviews/new_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe 'As a user', type: :feature do
   describe 'When I visit a Book review page' do
     before :each do
       @user = User.create!(name: "Billy")
-      @author = Auther.create!(name: "Johnny Flapjacks")
+      @author = Author.create!(name: "Johnny Flapjacks")
       @book = @author.books.create!(title: "The Flour Revolution", pages: 155, year: 1999)
     end
 
     it 'has a form for review information' do
-      visit review_path(@book)
+      visit new_book_review_path(@book)
 
       expect(page).to have_field("Title")
       expect(page).to have_field("Username")
