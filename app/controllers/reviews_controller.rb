537e8fa0eb441319999ac473544824b264758144
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-09 11:38:56 -0600

    Add book ivar to ReviewsCont#new, passed into Reviews/new.html form_for

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,6 @@
 class ReviewsController < ApplicationController
   def new
-    # require "pry"; binding.pry
-    # @new_review = Review.new(book_id: params[:book_id])
+    @book = Book.find(params[:book_id])
     @new_review = Review.new
-    # require "pry"; binding.pry
   end
 end
