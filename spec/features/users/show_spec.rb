374616e36aeb80ac48a033b4886cef5aa6cf31e8
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-10 10:32:16 -0600

    Add initial Users/show spec for seeing all reviews

---
+++ spec/features/users/show_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe 'As a user', type: :feature do
+  describe 'When I visit a Users show page' do
+    before :each do
+      @user = User.create!(name: "Anony-moose")
+
+      @author_1 = Author.create!(name: "John Flapjacks")
+
+      @book_1 = @author_1.books.create!(title: "Veronica Mars", pages: 10, year: 2012)
+      @book_2 = @author_1.books.create!(title: "Mars Aquatic", pages: 120, year: 1964)
+      @book_3 = @author_1.books.create!(title: "Trip to Mars", pages: 480, year: 2020)
+
+      @review_1 = @book_1.reviews.create!(title: "Wow!", text: "THIS BOOK IS AWESOME!", rating: 5, user: @user)
+      @review_2 = @book_2.reviews.create!(title: "Disappointing...", text: "This book seemed like it was more about drones than Mars.", rating: 2, user: @user)
+      @review_3 = @book_3.reviews.create!(title: "All the way home!", text: "This book went to the market!", rating: 3, user: @user)
+    end
+
+    it 'I should see all that users reviews' do
+      visit users_path(@user)
+
+      expect(page).to have_content("#{@user.name}")
+
+      within("##{@review_1.title}-review") do
+        expect(page).to have_content("#{@review_1.title}")
+        expect(page).to have_content("#{@review_1.rating}")
+        expect(page).to have_content("#{@review_1.text}")
+      end
+
+      within("##{@review_2.title}-review") do
+        expect(page).to have_content("#{@review_2.title}")
+        expect(page).to have_content("#{@review_2.rating}")
+        expect(page).to have_content("#{@review_2.text}")
+      end
+
+      within("##{@review_3.title}-review") do
+        expect(page).to have_content("#{@review_3.title}")
+        expect(page).to have_content("#{@review_3.rating}")
+        expect(page).to have_content("#{@review_3.text}")
+      end
+    end
+  end
+end
