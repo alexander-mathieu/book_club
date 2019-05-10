d8f4e91641213eda112b57bfaa1a8e2a3cab509c
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-10 17:34:10 -0600

    Update missing do end for the Book info it block in Author/show spec

--- spec/features/authors/show_spec.rb
+++ spec/features/authors/show_spec.rb
@@ -10,33 +10,33 @@ RSpec.describe 'As a user', type: :feature do
       @book_2 = @flapjacks.books.create!(title: "Mars Aquatic", pages: 120, year: 1964)
       @book_3 = @flapjacks.books.create!(title: "Trip to Mars", pages: 480, year: 2020)
       @book_1.authors << @cheese
-
     end
 
-    it 'I should be able to see all books and their information by that author'
-    visit author_path(@flapjacks)
+    it 'I should be able to see all books and their information by that author' do
+      visit author_path(@flapjacks)
 
-    expect(page).to have_content(@flapjacks.name)
+      expect(page).to have_content(@flapjacks.name)
 
-    within("#book-#{@book_1.id}-info") do
-      expect(page).to have_css("src=#{@book_1.image}")
-      expect(page).to have_content(@book_1.title)
-      expect(page).to have_content("Pages: #{@book_1.pages}")
-      expect(page).to have_content("Published: #{@book_1.year}")
-    end
+      within("#book-#{@book_1.id}-info") do
+        expect(page).to have_css("src=#{@book_1.image}")
+        expect(page).to have_content(@book_1.title)
+        expect(page).to have_content("Pages: #{@book_1.pages}")
+        expect(page).to have_content("Published: #{@book_1.year}")
+      end
 
-    within("#book-#{@book_2.id}-info") do
-      expect(page).to have_css("src=#{@book_2.image}")
-      expect(page).to have_content(@book_2.title)
-      expect(page).to have_content("Pages: #{@book_2.pages}")
-      expect(page).to have_content("Published: #{@book_2.year}")
-    end
+      within("#book-#{@book_2.id}-info") do
+        expect(page).to have_css("src=#{@book_2.image}")
+        expect(page).to have_content(@book_2.title)
+        expect(page).to have_content("Pages: #{@book_2.pages}")
+        expect(page).to have_content("Published: #{@book_2.year}")
+      end
 
-    within("#book-#{@book_3.id}-info") do
-      expect(page).to have_css("src=#{@book_3.image}")
-      expect(page).to have_content(@book_3.title)
-      expect(page).to have_content("Pages: #{@book_3.pages}")
-      expect(page).to have_content("Published: #{@book_3.year}")
+      within("#book-#{@book_3.id}-info") do
+        expect(page).to have_css("src=#{@book_3.image}")
+        expect(page).to have_content(@book_3.title)
+        expect(page).to have_content("Pages: #{@book_3.pages}")
+        expect(page).to have_content("Published: #{@book_3.year}")
+      end
     end
   end
 end
