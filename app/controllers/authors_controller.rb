a7af6716958f72f76797891c3237e7e1eb4687e6
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-11 13:39:14 -0600

    Add #destroy to AuthorsCont with flash.notice

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,4 +7,11 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
     @books = @author.books
   end
+
+  def destroy
+    @author = Author.destroy(params[:id])
+
+    flash.notice = "'#{@author.name}' was deleted."
+    redirect_to books_path
+  end
 end
