2b8a4aeeba0e35f73725d3d6f0312fd099961858
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-11 10:42:06 -0600

    Add initial #book_count spec for Author model

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -8,4 +8,17 @@ RSpec.describe Author, type: :model do
   describe "relationships" do
     it {should have_many(:books).through(:books_by_author)}
   end
+
+  describe 'instance methods' do
+    before :each do
+      @flapjacks = Author.create!(name: "John Flapjacks")
+
+      @book_1 = @flapjacks.books.create!(title: "Veronica Mars", pages: 10, year: 2012)
+      @book_2 = @flapjacks.books.create!(title: "Mars Aquatic", pages: 120, year: 1964)
+    end
+
+    it '#book_count' do
+      expect(@flapjacks.book_count).to eq(2)
+    end
+  end
 end
