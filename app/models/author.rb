0932fd0ffa04c231c82005787e4e462955a5cae1
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-11 14:00:50 -0600

    Add dependent: :destroy to books_by_author in Author, and book in BooksByAuthor to delete all Books attached to a deleted Author

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ApplicationRecord
-  has_many :books_by_author
+  has_many :books_by_author, dependent: :destroy
   has_many :books, through: :books_by_author
 
   validates_presence_of :name
