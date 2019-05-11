0151e07f2773a9326fe30f2b1ab6f9a4ff3ce495
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-05-11 11:20:24 -0600

    Implement top_three_users class method in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
   def review_count
     reviews.count
   end
+
+  def self.top_three_users
+    joins(:reviews)
+    .select('users.*, COUNT(reviews) AS review_count')
+    .group('users.id')
+    .order('review_count DESC')
+    .limit(3)
+  end
 end
