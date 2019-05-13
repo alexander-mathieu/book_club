4a8015b1454294938fa667808b554c1c6bf3bf0c
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-13 13:51:44 -0600

    Add unique user review per book if block for Reviews#create controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,11 +7,16 @@ class ReviewsController < ApplicationController
   def create
     user = User.find_or_create_by(name: params[:user][:name].titlecase)
     book = Book.find(params[:book_id])
-    new_review = user.reviews.new(review_params)
-    book.reviews << new_review
-    new_review.save
-
-    redirect_to books_path(book)
+    @new_review = user.reviews.new(review_params)
+    book.reviewers
+    if book.reviewers.include?(user.name)
+      flash.notice = "Looks like you've already reviewed this book!"
+    else
+      book.reviews << @new_review
+      @new_review.save
+      flash.notice = "New review '#{@new_review.title}' was added."
+    end
+    redirect_to book_path(book)
   end
 
   def destroy
