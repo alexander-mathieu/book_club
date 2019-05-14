4c60134cfde7b73fcd400d6a7c91fb1858a6cfed
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-05-13 18:25:01 -0600

    Add clause to set_default_image Book model method to account for nil value

--- app/models/book.rb
+++ app/models/book.rb
@@ -83,7 +83,7 @@ class Book < ApplicationRecord
   end
 
   def set_default_image
-    write_attribute(:image, '/assets/default_book.png') if self.image.empty?
+    write_attribute(:image, '/assets/default_book.png') if self.image.nil? || self.image.empty?
   end
 
 end
