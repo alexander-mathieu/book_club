9be755d50c458974ff5b9612e35ebf55914d6dcc
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-05-15 08:55:33 -0600

    Change default cover to default author

--- db/migrate/20190507182733_create_authors.rb
+++ db/migrate/20190507182733_create_authors.rb
@@ -2,7 +2,7 @@ class CreateAuthors < ActiveRecord::Migration[5.1]
   def change
     create_table :authors do |t|
       t.string :name
-      t.string :image, default: 'https://styluspub2.presswarehouse.com/publishers/default_cover.png'
+      t.string :image, default: 'https://torrentfreak.com/wp-content/themes/torrentfreakredux/assets/img/default-author.jpg'
 
       t.timestamps
     end
